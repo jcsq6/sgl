e61bd75e28f2002ae5c96745ad98d74ab7d2d968
JC Squires <50750105+jcsq6@users.noreply.github.com>
2023-05-28 20:09:07 -0400

    Delete build directory

--- build/contrib/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jcsqu/Dropbox/c++Dev/sgl/contrib/assimp/contrib/zlib
-# Build directory: C:/Users/jcsqu/Dropbox/c++Dev/sgl/build/contrib/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
